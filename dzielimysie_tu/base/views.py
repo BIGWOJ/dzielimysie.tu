b523aa4a3bd8bce5a32b6a908af92f65a4659c03
BIGWOJ <wojtek.latos@interia.pl>
2025-03-28 12:06:51 +0100

    Połączenie strony oferty, wydzielenie navbara i footera

--- dzielimysie_tu/base/views.py
+++ dzielimysie_tu/base/views.py
@@ -342,7 +342,6 @@ def take_offer(request, pk):
     offer.save()
     relation.save()
 
-    messages.success(request, 'Zgłoszenie zostało wysłane. Oczekuj na zatwierdzenie przez autora oferty.')
     return redirect('offer_page', pk=pk)
 
 def update_offer_status(request, pk, status, redirect_take_offers=False):
